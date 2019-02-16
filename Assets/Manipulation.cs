44dd750d84b8f0ecc309ade56be04f5b706f0b50
alancphilpott <alan.philpott@students.ittralee.ie>
2019-02-16 15:14:44 +0000

    Created floor material Created manipulation interface Created objectmanipulation implementation

---
+++ Assets/Manipulation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Manipulation : MonoBehaviour
+{
+    public abstract void move();
+    public abstract void scale();
+    public abstract void rotate();
+}
---
+++ Assets/ObjectManipulation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulation : Manipulation
+{
+    private float _rotationRate = 1f;
+
+    public override void move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void scale()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void rotate()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (Input.touches[0].phase == TouchPhase.Moved)
+            {
+                transform.Rotate(0, touch.deltaPosition.x * _rotationRate, 0, Space.World);
+            }
+        }
+    }
+}
