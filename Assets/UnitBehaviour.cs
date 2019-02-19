77076951dab84f4532b94bdd7848d3593c048ee9
alancphilpott <alan.philpott@students.ittralee.ie>
2019-02-19 03:16:31 +0000

    added unitbehaviour abstract script

---
+++ Assets/UnitBehaviour.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Defines default behaviour methods each unit must implement and perform
+ */
+
+using UnityEngine;
+
+public abstract class UnitBehaviour : MonoBehaviour
+{
+    public abstract void Move();
+    public abstract void Scale();
+    public abstract void Rotate();
+    public abstract void OnSelect();
+    public abstract void OnDeselect();
+}
