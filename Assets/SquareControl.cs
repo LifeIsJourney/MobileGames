e1be111bd8ae3e18513459091c0df498e27e704c
alancphilpott <alan.philpott@students.ittralee.ie>
2019-02-22 19:14:26 +0000

    cubes can now be moved when selected

--- Assets/SquareControl.cs
+++ Assets/SquareControl.cs
@@ -10,8 +10,7 @@ public class SquareControl : UnitBehaviour
     // private readonly float _rotateSpeed = 1f; // Unit rotation speed
     private bool _isRotating; // Track if unit is rotating
     private bool _isDragging; // Track if unit is being dragged
-    private Transform _toMove;
-    private float _distance;
+    private Vector3 _currentDragPos;
     
     void Start()
     {
@@ -26,29 +25,30 @@ public class SquareControl : UnitBehaviour
         {
             if (Input.touchCount == 1) // If one finger is touch
             {
+                _currentDragPos = Input.GetTouch(0).position;
                 if (Input.touches[0].phase == TouchPhase.Began && 
                     Physics.Raycast(_mainCamera.ScreenPointToRay(Input.touches[0].position), out _hit)) // If touch phase is began and it hits a unit
                 {
                     if (_hit.transform.CompareTag("SelectableUnit")) // If the unit is selectable
                     {
                         Debug.Log("Ready To Drag");
-
-                        _toMove = _hit.transform;
-                        _distance = _hit.transform.position.y - _mainCamera.transform.position.y;
                         _isDragging = true;
                     }
                 }
 
-                if (Input.touches[0].phase == TouchPhase.Moved && _isDragging)
+                if (Input.touches[0].phase == TouchPhase.Moved && _isDragging &&
+                    Physics.Raycast(_mainCamera.ScreenPointToRay(Input.touches[0].position), out _hit))
                 {
-                    Debug.Log("Moving");
+                    if (_hit.collider.CompareTag("SelectableUnit"))
+                    {
+                        Debug.Log("Moving");
+                        var newPos = _hit.point;
+                        var position = _cameraController.selectedUnit.transform.position;
+                        newPos.y = position.y;
 
-                    var touchWorldPoint = _mainCamera.ScreenToWorldPoint(Input.touches[0].position);
-                    var touchZ = touchWorldPoint.z;
-                    
-                    var position = new Vector3(Input.touches[0].position.x, _distance, touchZ);
-                    position = _mainCamera.ScreenToWorldPoint(position);
-                    _toMove.position = position;
+                        position = Vector3.Lerp(position, newPos, 5f);
+                        _cameraController.selectedUnit.transform.position = position;
+                    }
                 }
 
                 if ((Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Ended) &&
