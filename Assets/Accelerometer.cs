d176fced2d8b38569cd723dbcfbc245e87c41f32
alancphilpott <alan.philpott@students.ittralee.ie>
2019-02-23 06:08:25 +0000

    added accelerometer script

---
+++ Assets/Accelerometer.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Script for enabling tilt
+ */
+
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody _rigidBody;
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var tilt = Input.acceleration;
+
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+        
+        _rigidBody.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.red);
+    }
+}
