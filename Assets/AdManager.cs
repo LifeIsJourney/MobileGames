ec14ee598e0aebc559795da8879f83a0156b91b4
alancphilpott <alan.philpott@students.ittralee.ie>
2019-03-31 20:09:52 +0100

    test advertisements integrated

---
+++ Assets/AdManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    [SerializeField] private const string GameId = "3099945";
+
+    private void Awake()
+    {
+        Advertisement.Initialize(GameId, true);
+    }
+
+    public void ShowAd()
+    {
+        #if UNITY_EDITOR
+        StartCoroutine(WaitForAd());
+        #endif
+        
+        if (Advertisement.IsReady())
+        {
+            var options = new ShowOptions();
+            options.resultCallback = HandleAdResult;
+            
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleAdResult(ShowResult result)
+    {
+        switch(result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Added 5 Gems to Account");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Player Skipped Ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Player Failed to Launch Ad - Internet?");
+                break;
+        }
+    }
+    
+    IEnumerator WaitForAd()
+    {
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+            yield return null;
+
+        Time.timeScale = currentTimeScale;
+    }
+}
