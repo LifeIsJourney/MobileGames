c8e226694a25f6560d6421556d2432ec73bb4fec
alancphilpott <alan.philpott@students.ittralee.ie>
2019-04-01 03:32:23 +0100

    Updated game to larger scale Added sphere (chaser) which chases the cube you move

---
+++ Assets/SphereControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour
+{
+    private GameObject targetObject = null;
+
+    void Start()
+    {
+        targetObject = GameObject.Find("CubeTwo");
+        Debug.Log(targetObject.transform.position);
+    }
+    
+    void Update()
+    {
+        targetObject = GameObject.Find("CubeTwo");
+
+        transform.position = Vector3.MoveTowards(transform.position, targetObject.transform.position, 0.3f);
+    }
+}
