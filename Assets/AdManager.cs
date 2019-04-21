fdf168a230ad9f08ffc450fbf8ae01ae073145f3
alancphilpott <alan.philpott@students.ittralee.ie>
2019-04-21 16:01:15 +0100

    Added Gitignore
    This Unity gitignore file will determine which files are uploaded to the repo.

--- Assets/AdManager.cs
+++ Assets/AdManager.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
+using GoogleMobileAds.Api;
 using UnityEngine.Advertisements;
 using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
     [SerializeField] private const string GameId = "3099945";
+    private InterstitialAd _interstitial;
+    private string _adUnitID;
 
     private void Awake()
     {
         Advertisement.Initialize(GameId, true);
+        RequestInterstitialAds();
     }
 
     public void ShowAd()
@@ -24,6 +28,23 @@ public class AdManager : MonoBehaviour
             
             Advertisement.Show("rewardedVideo", options);
         }
+    }
+
+    public void showInterstitialAd()
+    {
+        if (_interstitial.IsLoaded())
+        {
+            _interstitial.Show();
+        }
+    }
+    
+    private void RequestInterstitialAds()
+    {
+        #if UNITY_ANDROID
+            _adUnitID = "ca-app-pub-3940256099942544/1033173712";
+        #endif
+        
+        _interstitial = new InterstitialAd(_adUnitID);
     }
 
     private void HandleAdResult(ShowResult result)
