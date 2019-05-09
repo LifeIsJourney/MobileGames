5632af88494a3da1e7adff5557e4cc593eddd140
alancphilpott <alan.philpott@students.ittralee.ie>
2019-05-09 23:50:41 +0100

    Added missing scripts

---
+++ Assets/PlayGames.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class PlayGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        
+        SignIn();
+    }
+
+    private static void SignIn()
+    {
+        Social.localUser.Authenticate(success => { });
+    }
+
+    #region  Achievements
+
+    public static void UnlockAchievement(string id)
+    {
+        Social.ReportProgress(id, 100, success => { });
+    }
+
+    public void ShowAchievementsUi()
+    {
+        Social.ShowAchievementsUI();
+    }
+    
+    #endregion
+    
+    #region Leaderboards
+
+    public static void AddScoreToLeaderboard(string leaderboardId, long score)
+    {
+        Social.ReportScore(score, leaderboardId, success => { });
+    }
+
+    public void ShowLeaderboardUi()
+    {
+        Social.ShowLeaderboardUI();
+    }
+    
+    #endregion
+}
---
+++ Assets/ScoreControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreControl : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private Text _highScoreText;
+    
+
+    private int _score;
+    private int _highScore;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _highScoreText = GameObject.Find("HighScoreText").GetComponent<Text>();
+        scoreText.text = "Score: " + 0;
+
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
+        _highScoreText.text = "High Score: " + _highScore;
+        InvokeRepeating(nameof(UpdateScores), 1f, 1f);
+    }
+
+    private void UpdateScores()
+    {
+        _score += 1;
+        if (_score > _highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", _score);
+            _highScoreText.text = "High Score: " + _score;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        scoreText.text = "Score: " + _score;
+    }
+}
