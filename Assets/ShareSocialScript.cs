15975ca48247f3170f7aa7c85156a3a610f62930
alancphilpott <alan.philpott@students.ittralee.ie>
2019-05-10 20:28:15 +0100

    Added simple implementation of sharing to Twitter

--- Assets/ShareSocialScript.cs
+++ Assets/ShareSocialScript.cs
@@ -7,16 +7,30 @@ public class ShareSocialScript : MonoBehaviour
     #region Facebook Variables
 
     private string appID = "1039972606390912";
-    private string link = "https://www.facebook.com/feed?";
+    private string fb_link = "https://www.facebook.com/feed?";
     private string picture = "https://cdn2.iconfinder.com/data/icons/flat-game-ui-buttons-icons-1/512/19-512.png";
-    private string caption = "Check Out My New High Score: ";
+    private string caption = "Check Out My New High Score on Chaser: ";
     private string description = "Enjoy fun free games. Challenge Yourself & Share With Friends";
+
+    #endregion
+
+    #region Twitter Variables
+
+    private string tw_link = "http://twitter.com/intent/tweet";
+    private string tw_language = "en";
+    private string tw_text = "Check Out My New High Score on Chaser: ";
 
     #endregion
 
     public void ShareScoreFacebook()
     {
         Debug.Log("Sharing High Score: " + PlayerPrefs.GetInt("HighScore", 0) + " to Facebook");
-        Application.OpenURL(link + "app_id=" + appID + "&link=" + link + "&picture=" + picture + "&caption=" + caption + PlayerPrefs.GetInt("HighScore", 0) + "&description=" + description);
+        Application.OpenURL(fb_link + "app_id=" + appID + "&link=" + fb_link + "&picture=" + picture + "&caption=" + caption + PlayerPrefs.GetInt("HighScore", 0) + "&description=" + description);
+    }
+
+    public void ShareScoreTwitter()
+    {
+        Debug.Log("Sharing High Score: " + PlayerPrefs.GetInt("HighScore", 0) + " to Twitter");
+        Application.OpenURL(tw_link + "?text=" + WWW.EscapeURL(tw_text) + PlayerPrefs.GetInt("HighScore", 0) + "&amp;lang=" + WWW.EscapeURL(tw_language));
     }
 }
