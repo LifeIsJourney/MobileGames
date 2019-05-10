21e0b20460edd7cd356ef3f9069c780c2afd84d0
alancphilpott <alan.philpott@students.ittralee.ie>
2019-05-10 07:42:39 +0100

    Add facebook manager script

---
+++ Assets/FacebookManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FacebookManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Could Not Initialise App");
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+    
+    #region Login/Logout
+
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions);
+    }
+
+    public void FacebookLogout()
+    {
+        FB.LogOut();
+    }
+    
+    #endregion
+
+    public void FacebookShare()
+    {
+        FB.ShareLink(new System.Uri(""));
+    }
+}
